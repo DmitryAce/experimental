82b805290ad873cf6c994dc36d73c813147e9a56
Dmitry Borzov <47420776+DmitryAce@users.noreply.github.com>
2024-07-12 19:45:58 +0300

    Delete directory

--- replacer/replacer.py
+++
@@ -1,8 +0,0 @@
-with open("text.txt", "r+", encoding='utf-8') as f:
-    lines = f.readlines() 
-    f.seek(0)
-    for line in lines:
-        line = line.replace("*", "") 
-        f.write(line) 
-
-    f.truncate()
